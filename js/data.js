8b195fea36a12f541f5d55f387924796ef7ae3ed
北小齐 <1465583644@qq.com>
2025-01-09 21:07:28 +0800

    更新UI

---
+++ js/data.js
@@ -0,0 +1,62 @@
+// 作者：北小齐    http://bxiaoqi.netlify.app/
+
+/**
+ * 描述： 用户信息数据储存全部用户信息
+ * 参数  username： 用户名
+ * 参数  password： 密码
+ * 参数  email： 邮箱
+ * 参数  permiss： 权限 0：游客 1：普通用户 2：管理员 3：超级管理员
+ * 参数  avatar： 头像
+ */
+
+// 用户数据
+var userdata = [
+    { username: "北小齐", password: "127769", email: "1465583644@qq.com", permiss: 3, avatar: "./images/userAvatar/TX_1.png" },
+    { username: "BxiaoQi", password: "127769", email: "1465583644@qq.com", permiss: 1, avatar: "./images/userAvatar/TX_1.png" },
+]
+
+/**
+ * 模拟数据库
+ * 参数  name： 名称
+ * 参数  desc： 描述
+ * 参数  ico： 图标
+ * 参数  src： 链接
+ * 参数  permiss： 权限
+ */
+var data = {
+    tools: {
+        name: "在线工具",
+        data: [
+            { ico: "./images/appicon/icon.png", name: "BMI计算器", src: "./tool/BMI/index.html", desc: "计算体质指数检测你是否是真的胖（结果数据由世界卫生组织定制）", permiss: 0 },
+            { ico: "./images/appicon/icon.png", name: "项目2", src: "操作2", desc: "描述2", permiss: 1 },
+        ]
+    },
+    video: {
+        name: "在线影视",
+        data: [
+            { ico: "./images/appicon/icon.png", name: "项目3", src: "操作3", desc: "描述3", permiss: 0 },
+            { ico: "./images/appicon/icon.png", name: "项目4", src: "操作4", desc: "描述4", permiss: 1 },
+        ]
+    },
+    music: {
+        name: "在线音乐",
+        data: [
+            { ico: "./images/appicon/icon.png", name: "项目5", src: "操作5", desc: "描述5", permiss: 0 },
+            { ico: "./images/appicon/icon.png", name: "项目6", src: "操作6", desc: "描述6", permiss: 1 },
+        ]
+    },
+    progm: {
+        name: "实用程序",
+        data: [
+            { ico: "./images/appicon/icon.png", name: "项目7", src: "操作7", desc: "描述7", permiss: 1 },
+            { ico: "./images/appicon/icon.png", name: "项目8", src: "操作8", desc: "描述8", permiss: 1 },
+        ]
+    },
+    hides: {
+        name: "隐藏功能",
+        data: [
+            { ico: "./images/appicon/icon.png", name: "项目9", src: "操作9", desc: "描述9", permiss: 2 },
+            { ico: "./images/appicon/icon.png", name: "项目10", src: "操作10", desc: "描述10", permiss: 1 },
+        ]
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,3 +1,111 @@
-document.getElementById('baiduButton').addEventListener('click', function() {
-    window.location.href = 'https://www.baidu.com';
-});
+
+var userObj = { username: "游客", password: "123456", email: "无", permiss: 0, avatar: "./images/userAvatar/tourist.png" }
+// 自执行函数
+!function () {
+    let user = document.getElementById("username"); // 获取用户名元素
+    let userstr = localStorage.getItem("user"); // 获取用户信息
+    let avatar = document.getElementById("avatar"); // 获取用户头像元素
+    if (JSON.parse(userstr)) {
+        userObj = JSON.parse(userstr); // 将字符串转为对象
+    }
+    user.innerText = "欢迎您！" + userObj.username; // 显示用户名
+    avatar.src = userObj.avatar;
+    show("all")
+    switchlogin()
+}();
+
+// 显示菜单
+function expand() {
+    let checkbox = document.getElementById("menuSwitch");
+    checkbox.checked = !checkbox.checked;
+    if (checkbox.checked) {
+        document.getElementById("content").style.left = "200px";
+    } else {
+        document.getElementById("content").style.left = "70px";
+    }
+}
+
+// 是否显示隐藏功能
+function hides() {
+    if (userObj.permiss == 0) {
+        var result = confirm("隐藏功能需要登录，是否跳转登录?");
+        if (result) {
+            window.location.href = "./view/login.html";
+        }
+    } else {
+        show("hides")
+    }
+}
+
+// 切换登录图标
+function switchlogin() {
+    let loginIconObj = document.getElementById("loginIcon");
+    let loginTextObj = document.getElementById("loginText");
+    if (userObj.permiss == 0) {
+        loginIconObj.src = "./images/icon/login.png";
+        loginTextObj.innerText = "登录账号";
+    } else {
+        loginIconObj.src = "./images/icon/logout.png";
+        loginTextObj.innerText = "退出登录";
+    }
+}
+
+// 退出登录
+function login() {
+    if (userObj.permiss == 0) {
+        window.location.href = "./view/login.html";
+    } else {
+        var result = confirm("确定退出登录吗？");
+        if (result) {
+            localStorage.removeItem("user");
+            window.location.href = "./index.html";
+        }
+    }
+}
+
+
+// 显示数据
+function show(group) {
+    let tbody = document.getElementById("tbody")
+    let title = document.getElementById("titlename");
+    tbody.innerHTML = "";
+    let htmlstr = "";
+    if (group == "all") {
+        let allkeys = Object.keys(data);
+        for (let i = 0; i < allkeys.length; i++) {
+            let key = allkeys[i];
+            let value = data[key].data;
+            for (let j = 0; j < value.length; j++) {
+                htmlstr += jointItem(value[j]);
+            }
+        }
+        title.innerText = "北小齐珍藏";
+    } else {
+        let value = data[group].data;
+        for (let i = 0; i < value.length; i++) {
+            htmlstr += jointItem(value[i])
+        }
+        title.innerText = data[group].name;
+    }
+    tbody.innerHTML = htmlstr;
+}
+
+// 拼接单项数据
+function jointItem(obj) {
+    let str = ""
+    if (userObj.permiss >= obj.permiss) {
+        str = `
+            <tr>
+                <td><img src="${obj.ico}" alt="ico"></td>
+                <td>${obj.name}</td>
+                <td>${obj.desc}</td>
+                <td>
+                    <a href="">收藏</a>
+                    <a href="${obj.src}" target="_blank">看看去</a>
+                </td>
+            </tr>
+            `;
+    }
+
+    return str;
+}
